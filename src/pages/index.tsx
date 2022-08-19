d3e2aea57d7d0ba59b357f9f63684726c96209dd
Mohamad Sleiman Haidar <mhaidar@myki.com>
2022-08-19 17:39:45 +0300

    chore: remove use effect in pages/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Index = () => {
   })
 
   const [{ data: searchResults }] = useTestQueryQuery()
-
+  //
   if (!fetching && !data) {
     return (
       <div>
@@ -33,8 +33,8 @@ const Index = () => {
     )
   }
   useTestQueryQuery()
-
-  useEffect(() => {}, [])
+  //
+  // useEffect(() => {}, [])
 
   return (
     <Layout>
