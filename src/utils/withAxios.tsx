e0fbe11157139b8c329d8b80c90686e9874a8304
msh <mohamad.sleimanhaidar@gmail.com>
2024-01-31 01:29:18 +0200

    set res cookie

--- src/utils/withAxios.tsx
+++ src/utils/withAxios.tsx
@@ -10,7 +10,7 @@ const createAxiosInstance = (ctx?: NextPageContext) => {
     headers: {
       ...(typeof window === 'undefined' && ctx?.req?.headers.cookie
         ? { Cookie: ctx.req.headers.cookie }
-        : { Cookie: ctx?.req?.headers.cookie }),
+        : {}),
     },
   })
 
