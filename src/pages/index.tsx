ee39f660b8b48c36b085e44aacc7cd36437b1f4f
msh <mohamad.sleimanhaidar@gmail.com>
2024-02-07 17:50:45 +0200

    make noon type animation initial loading indicator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router'
 import { isServer } from '../utils/isServer'
 import { withAxios } from '../utils/withAxios'
 import Head from 'next/head'
+import { TypeAnimation } from 'react-type-animation'
 
 const meta = {
   title: 'Noon – Open source, secure, free communication platform.',
@@ -49,6 +50,22 @@ const Index = ({ axios }) => {
     }
   }, [userData, loading, router])
 
+  const generateRandomSequence = () => {
+    const sequence = [
+      'N',
+      randomWait(),
+      'NO',
+      randomWait(),
+      'NOO',
+      randomWait(),
+      'NOON',
+      2000,
+    ]
+    return sequence
+  }
+
+  const randomWait = () => Math.floor(Math.random() * (1250 - 300 + 1)) + 300
+
   return (
     <>
       <Head>
@@ -56,7 +73,23 @@ const Index = ({ axios }) => {
       </Head>
 
       <Flex className="flex-col justify-center items-center bg-red-500 text-black h-screen">
-        {loading && <p className="fixed top-12 text-5xl">Loading...</p>}
+        {/*{loading && <p className="fixed top-12 text-5xl">Loading...</p>}*/}
+
+        {loading && (
+          <p className="fixed top-12 text-4xl text-red-500 leading-tight">
+            <TypeAnimation
+              sequence={generateRandomSequence()}
+              wrapper="span"
+              speed={50}
+              style={{
+                fontSize: '1.5em',
+                display: 'inline-block',
+                whiteSpace: 'pre-line',
+              }}
+              repeat={Infinity}
+            />
+          </p>
+        )}
       </Flex>
     </>
   )
