e2280c959c738b6a34ce3f0f5ec85f0563eed720
msh <mohamad.sleimanhaidar@gmail.com>
2024-02-08 11:28:31 +0200

    use isAuth hook in _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,12 @@
 import { Flex } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Reactfrom 'react'
 import Head from 'next/head'
 import { TypeAnimation } from 'react-type-animation'
-
-import { useGetMeQuery } from '../store/api/usersApiSlice'
-import { setLoggedInUser } from '../store/users'
-import { useDispatch } from 'react-redux'
 
 const meta = {
   title: 'Noon – Open source, secure, free communication platform.',
 }
 const Index = () => {
-  const router = useRouter()
-  const dispatch = useDispatch()
-  const { data: user, isLoading } = useGetMeQuery(undefined)
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (user?.username) {
-        dispatch(setLoggedInUser(user))
-        router.replace('/noon')
-      } else {
-        router.replace('/onboarding')
-      }
-    }
-  }, [user, isLoading, router])
-
   const generateRandomSequence = (): (string | number)[] => [
     'N',
     randomWait(),
@@ -48,21 +28,19 @@ const Index = () => {
       </Head>
 
       <Flex className="flex-col justify-center items-center bg-black text-black h-screen">
-        {isLoading && (
-          <p className="fixed top-12 text-4xl text-red-500 leading-tight">
-            <TypeAnimation
-              sequence={generateRandomSequence()}
-              wrapper="span"
-              speed={50}
-              style={{
-                fontSize: '1.5em',
-                display: 'inline-block',
-                whiteSpace: 'pre-line',
-              }}
-              repeat={Infinity}
-            />
-          </p>
-        )}
+        <p className="fixed top-12 text-4xl text-red-500 leading-tight">
+          <TypeAnimation
+            sequence={generateRandomSequence()}
+            wrapper="span"
+            speed={50}
+            style={{
+              fontSize: '1.5em',
+              display: 'inline-block',
+              whiteSpace: 'pre-line',
+            }}
+            repeat={Infinity}
+          />
+        </p>
       </Flex>
     </>
   )
